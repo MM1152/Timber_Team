3e8634a60434836050cb08709407796126a704c5
천민성 <albert6306@naver.com>
2025-07-01 17:21:37 +0900

    나무 위치설정 완료

--- sfml-timber2/SceneMuiltGame.cpp
+++ sfml-timber2/SceneMuiltGame.cpp
@@ -8,7 +8,8 @@ SceneMuiltGame::SceneMuiltGame()
 
 void SceneMuiltGame::Init()
 {
-
+	texIds.push_back(IMG"background.png");
+	texIds.push_back(IMG"tree.png");
 	Scene::Init();
 }
 
@@ -20,19 +21,32 @@ void SceneMuiltGame::Release()
 
 void SceneMuiltGame::Enter()
 {
-	texIds.push_back(IMG"background.png");
-
 	SpriteGo* backGround1 = new SpriteGo(IMG"background.png");
 	SpriteGo* backGround2 = new SpriteGo(IMG"background.png");
+	SpriteGo* tree1 = new SpriteGo(IMG"tree.png");
+	SpriteGo* tree2 = new SpriteGo(IMG"tree.png");
+
+	sf::FloatRect windowSize = FRAMEWORK.GetWindowBounds();
 
 	backGround1->SetScale({ 0.49f , 1.f });
-	std::cout << backGround1->GetOrigin().x << std::endl;
-	backGround2->SetScale({ 0.49f , 1.f });
-	backGround2->SetOrigin({ -FRAMEWORK.GetWindowBounds().width - 60.f , 0.f });
 
+	backGround2->SetScale({ 0.49f , 1.f });
+	backGround2->SetPosition({ (windowSize.width /  2) + 20 , 0.f});
+	
 	AddGameObject(backGround1);
 	AddGameObject(backGround2);
+	AddGameObject(tree1);
+	AddGameObject(tree2);
 	Scene::Enter();
+
+	tree1->SetOrigin(Origins::TC);
+	tree1->SetScale({ 0.5, 1.f });
+	tree1->SetPosition({ (backGround1->GetSprite().getLocalBounds().width * backGround1->GetScale().x) / 2 , 0.f });
+
+	tree2->SetOrigin(Origins::TC);
+	tree2->SetScale({ 0.5, 1.f });
+	std::cout << backGround2->GetPosition().x << std::endl;	
+	tree2->SetPosition({ (backGround2->GetSprite().getLocalBounds().width * backGround2->GetScale().x + backGround2->GetPosition().x / 2), 0.f});
 }
 
 void SceneMuiltGame::Exit()

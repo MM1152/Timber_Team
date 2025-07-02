be674165d98ec1011503deac476859fc20433733
천민성 <albert6306@naver.com>
2025-07-02 18:01:02 +0900

    아이템 기능 구현필요함

---
+++ sfml-timber2/Item.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "Item.h"
+
+void Item::Init()
+{
+}
+
+void Item::Release()
+{
+}
+
+void Item::Reset()
+{
+}
+
+void Item::Update(float dt)
+{
+}
+
+void Item::Draw(sf::RenderWindow& window)
+{
+}
--- sfml-timber2/Item.h
+++ sfml-timber2/Item.h
@@ -3,5 +3,15 @@
 #include "GameObject.h"
 class Item : public GameObject
 {
+protected:
+	sf::Sprite sp;
+public:
+	// GameObject을(를) 통해 상속됨
+	void Init() override;
+	void Release() override;
+	void Reset() override;
+	void Update(float dt) override;
+	void Draw(sf::RenderWindow& window) override;
+
 };
 
